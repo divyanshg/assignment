11c7d594798fadb84843bb24a7a01b26048b3c69
divyanshg <divyanshg809@gmail.com>
2022-10-31 19:58:53 +0530

    port change

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get("/content", async (req, res) => {
 
 async function startServer() {
   await connectToDB();
-  app.listen(3000, () => console.log("Server Started"));
+  app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
 }
 
 startServer();
